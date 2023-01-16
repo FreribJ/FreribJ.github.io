599c165919204c32236b2b87d618990c79d53767
Jannes Frenker <frerib.j@gmail.com>
2023-01-16 15:32:54 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {Router} from '@angular/router';
+
+interface Site {
+  path: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+  constructor(private router: Router) {
+  }
   title = 'Angular';
+
+  ngOnInit(): void {
+    this.switchSite();
+  }
+
+  sites: Site[] = [
+    {path: 'standard', name: 'Standard'},
+    {path: 'withError', name: 'Mit Fehlern'},
+    {path: 'complex', name: 'Komplex'},
+  ]
+  selectedSite: Site = this.sites[2];
+  switchSite() {
+    this.router.navigate([this.selectedSite.path]);
+  }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {FormsModule} from "@angular/forms";
+import { WithErrorComponent } from './with-error/with-error.component';
+import { StandardComponent } from './standard/standard.component';
+import {RouterModule} from "@angular/router";
+import { ComplexComponent } from './complex/complex.component';
+import {MatSelectModule} from "@angular/material/select";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WithErrorComponent,
+    StandardComponent,
+    ComplexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSlideToggleModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'standard', component: StandardComponent},
+      {path: 'withError', component: WithErrorComponent},
+      {path: 'complex', component: ComplexComponent},
+    ]),
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
